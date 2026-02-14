08412ad8dbda9119787ed303bd59abfd1544f986
itsakuro <mockup345@gmail.com>
2026-02-13 22:04:25 -0600

    initial commit !!!

---
+++ src/main/java/lol/squiggle/cdrm/CentralDesignRadioModClient.java
@@ -0,0 +1,10 @@
+package lol.squiggle.cdrm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CentralDesignRadioModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
